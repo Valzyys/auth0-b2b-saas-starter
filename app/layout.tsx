d7890a7cb83b02c4f6b59372234ceeba1f568b86
Valzz <jkt48connect@gmail.com>
2026-06-10 04:14:27 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next"
-
 import "./globals.css"
-
 import { Inter } from "next/font/google"
 import Script from "next/script"
-
 import { Toaster } from "@/components/ui/sonner"
 import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "SaaStart | Auth0 by Okta",
+  title: "T48ID | Platform Live Stream Theater JKT48",
   description:
-    "SaaStart is a reference B2B SaaS application built using Next.js and Auth0 by Okta.",
-  metadataBase: new URL("https://saastart.app"),
+    "T48ID adalah platform terbaik untuk menonton dan membeli tiket live stream theater JKT48 dengan kualitas streaming terbaik.",
+  metadataBase: new URL("https://t48idlive.my.id"),
 }
 
 export default async function RootLayout({
@@ -23,7 +20,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="id" className={inter.className} suppressHydrationWarning>
       <body className="min-h-screen bg-background font-sans antialiased">
         <ThemeProvider
           attribute="class"
@@ -33,7 +30,6 @@ export default async function RootLayout({
         >
           {children}
         </ThemeProvider>
-
         <Toaster position="bottom-right" />
         <Script id="heap">
           {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
